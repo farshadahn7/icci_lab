6e04999bcdd60bf894d1118a19c4163b60152c30
it <it@i-bmn.ir>
2024-08-04 09:58:48 +0330

    made special template tag for latest post so i could load it in all web pages

---
+++ posts/templatetags/post_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from posts.models import Post
+
+register = template.Library()
+
+
+@register.inclusion_tag('latest_posts.html')
+def latest_posts():
+    posts = Post.objects.order_by('-created_at')[:2]
+    return {'latest_posts': posts}
