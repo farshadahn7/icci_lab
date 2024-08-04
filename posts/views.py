f6ff199f8ddf2da12e4544ad19a8e7efe37a3525
it <it@i-bmn.ir>
2024-08-04 09:48:25 +0330

    lates posts of blog add and the date of post configured

--- posts/views.py
+++ posts/views.py
@@ -10,3 +10,8 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog.html'
     context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context_object_name = super().get_context_data(**kwargs)
+        context_object_name['latest_posts'] = Post.objects.order_by('-created_at')[0:2]
+        return context_object_name
