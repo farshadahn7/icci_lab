2e498cbde73692e5816024b11d21f5f2795d2988
farshadahn7 <farshadahn7@gmail.com>
2024-08-07 00:30:37 +0330

    now publication is downloadble

--- publication/models.py
+++ publication/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Publication(models.Model):
     title = models.CharField(max_length=384)
+    file = models.FileField(upload_to='publications/', default=None, null=True, blank=True)
     author = models.CharField(max_length=384)
     published_at = models.CharField(max_length=384)
     published_at_detail = models.CharField(max_length=512, default='test')
