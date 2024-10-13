7219af8d9d549ed06919fc76581d883be4c8511b
it <it@i-bmn.ir>
2024-10-13 09:02:47 +0330

    fixed the status bug and fixed the number of cat show in category

--- category/templatetags/category_tag.py
+++ category/templatetags/category_tag.py
@@ -1,13 +1,14 @@
 from django import template
 from category.models import Category
 from django.db.models import Count
+from django.db.models import Q
 
 register = template.Library()
 
 
 @register.inclusion_tag('category_list.html')
 def category_list():
-    categories = Category.objects.annotate(number_of_cat=Count("posts")).all()
+    categories = Category.objects.annotate(number_of_cat=Count("posts", filter=Q(posts__status=True))).all()
     category_lists = Category.objects.all()
     cat_list = []
     for category in categories:
